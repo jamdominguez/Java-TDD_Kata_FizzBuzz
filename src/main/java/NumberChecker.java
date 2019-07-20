0cabacdfc0c9c8a96722746be028334e42851f95
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2019-07-20 17:09:46 +0200

    refactor and comment

--- src/main/java/NumberChecker.java
+++ src/main/java/NumberChecker.java
@@ -1,11 +1,15 @@
 public class NumberChecker {
-    public static String fizzBuzz(int i) {
-        if (i == 0) return "0";
-        StringBuilder result = new StringBuilder();
-        if (i%3 == 0) result.append("Fizz");
-        if (i%5 == 0) result.append("Buzz");
-        if (result.length() == 0) result.append(i);
 
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+
+    public static String fizzBuzz(int i) {
+        StringBuilder result = new StringBuilder();
+
+        if (i != 0 && i % 3 == 0) result.append(FIZZ); //not 0 and divisible by 3
+        if (i != 0 && i % 5 == 0) result.append(BUZZ); //not 0 and divisible by 5
+        if (result.length() == 0) result.append(i); //other case return the number
+        
         return result.toString();
     }
 }
